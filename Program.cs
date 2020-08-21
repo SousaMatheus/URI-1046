5af99159b04d10883ea164599e3b4c807ac56c9f
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-08-20 22:24:38 -0300

    Criado primeiro código

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace URI_1046
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int horaInicial, horaFinal, duracaoJogo ;
+            string [] var = Console.ReadLine().Split(' ');
+            horaInicial = int.Parse[0];
+            horaFinal = int.Parse[0];
+
+            if ( horaInicial < horaFinal){
+                duracaoJogo = horaFinal - horaInicial;
+                    Console.Log($"O JOGO DUROU {duracaoJogo} HORA(S)");
+            } 
+            else {
+                duracaoJogo = 24 - horaInicial + horaFinal;
+                    Console.Log($"O JOGO DUROU {duracaoJogo} HORA(S)");
+            }
         }
     }
 }
