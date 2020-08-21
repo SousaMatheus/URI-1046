173b2ebe0eb959b7cab2e15e5156c4ea0c5d685e
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-08-20 21:52:28 -0300

    Criei aplicação URI 1046

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace URI_1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
